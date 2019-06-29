5bac4e7449de7769d67965f000956aa70fa9d7e8
tyleung <tyleung@ualberta.ca>
2019-06-29 13:53:11 +0800

    initial commit

---
+++ mobile/navigation/AppNavigator.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { createAppContainer, createStackNavigator } from 'react-navigation';
+import OnboardingScreen from '../screens/OnboardingScreen';
+import SignUpScreen from '../screens/SignUpScreen';
+
+export default createAppContainer(
+  createStackNavigator(
+    {
+      // You could add another route here for authentication.
+      // Read more at https://reactnavigation.org/docs/en/auth-flow.html
+      Onboarding: { screen: OnboardingScreen },
+      SignUp: { screen: SignUpScreen }
+    },
+    {
+      headerMode: 'none'
+    }
+  )
+);
---
+++ mobile/screens/SignUpScreen.js
@@ -0,0 +1,159 @@
+import React, { Component } from 'react';
+import {
+  StyleSheet,
+  View,
+  TouchableOpacity,
+  Text,
+  Image,
+  ImageBackground
+} from 'react-native';
+import InputField from '../components/InputField';
+import LoginButton from '../components/LoginButton';
+
+const companyLogo = require('../images/PepeHands.png');
+const email = require('../images/PepeHands.png');
+const password = require('../images/PepeHands.png');
+
+import { Dimensions } from 'react-native';
+const { height, width } = Dimensions.get('window');
+export const w = percent => (width * percent) / 100;
+export const h = percent => (height * percent) / 100;
+export const totalSize = num =>
+  (Math.sqrt(height * height + width * width) * num) / 100;
+
+export default class SignUpScreen extends Component {
+  state = {
+    isEmailCorrect: false,
+    isPasswordCorrect: false,
+    isLogin: false
+  };
+
+  getStarted = () => {
+    const email = this.email.getInputValue();
+    const password = this.password.getInputValue();
+
+    this.setState(
+      {
+        isEmailCorrect: email === '',
+        isPasswordCorrect: password === ''
+      },
+      () => {
+        if (email !== '' && password !== '') {
+          this.loginToFireBase(email, password);
+        } else {
+          console.warn('Fill up all fields');
+        }
+      }
+    );
+  };
+
+  changeInputFocus = name => () => {
+    if (name === 'Email') {
+      this.setState({ isEmailCorrect: this.email.getInputValue() === '' });
+      this.password.input.focus();
+    } else {
+      this.setState({
+        isPasswordCorrect: this.password.getInputValue() === ''
+      });
+    }
+  };
+
+  loginToFireBase = (email, password) => {
+    this.setState({ isLogin: true });
+    Firebase.userLogin(email, password).then(user => {
+      if (user) this.props.success(user);
+      this.setState({ isLogin: false });
+    });
+  };
+
+  render() {
+    const { navigate } = this.props.navigation;
+
+    return (
+      <ImageBackground
+        source={require('../images/greenforest.png')}
+        style={{ width: '100%', height: '100%' }}
+      >
+        <View style={styles.container}>
+          <Image
+            style={styles.icon}
+            resizeMode="contain"
+            source={companyLogo}
+          />
+          <InputField
+            placeholder="Email"
+            keyboardType="email-address"
+            style={styles.email}
+            error={this.state.isEmailCorrect}
+            focus={this.changeInputFocus}
+            ref={ref => (this.email = ref)}
+            icon={email}
+          />
+          <InputField
+            placeholder="Password"
+            returnKeyType="done"
+            secureTextEntry={true}
+            blurOnSubmit={true}
+            error={this.state.isPasswordCorrect}
+            ref={ref => (this.password = ref)}
+            focus={this.changeInputFocus}
+            icon={password}
+          />
+          <LoginButton click={this.getStarted} isLogin={this.state.isLogin} />
+          <View style={styles.textContainer}>
+            <TouchableOpacity
+              // onPress={navigate('register')}
+              style={styles.touchable}
+              activeOpacity={0.6}
+            >
+              <Text style={styles.createAccount}>Create Account</Text>
+            </TouchableOpacity>
+            <TouchableOpacity
+              // onPress={navigate('forgot')}
+              style={styles.touchable}
+              activeOpacity={0.6}
+            >
+              <Text style={styles.forgotPassword}>Forgot Password</Text>
+            </TouchableOpacity>
+          </View>
+        </View>
+      </ImageBackground>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center'
+  },
+  icon: {
+    width: w(70),
+    height: h(30),
+    marginTop: h(10),
+    marginBottom: h(7)
+  },
+  textContainer: {
+    width: w(100),
+    flexDirection: 'row',
+    marginTop: h(5)
+  },
+  email: {
+    marginBottom: h(4.5)
+  },
+  touchable: {
+    flex: 1
+  },
+  createAccount: {
+    color: '#ffffff',
+    textAlign: 'center',
+    fontSize: totalSize(2),
+    fontWeight: '600'
+  },
+  forgotPassword: {
+    color: '#ffffff',
+    textAlign: 'center',
+    fontSize: totalSize(2),
+    fontWeight: '600'
+  }
+});
